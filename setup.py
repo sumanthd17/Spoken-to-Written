a474409810910437b382b6ff9ef35dafb1246979
Sumanth <dsumanth17@gmail.com>
2019-10-26 14:22:29 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ from distutils.core import setup
 setup(
     name='spoken2written',
     version='0.1',
+    author='Sumanth Doddapaneni',
+    author_email='dsumanth17@gmail.com',
+    description='A package to convert spoken english to written english',
     packages=['spoken2written',],
+    url="https://github.com/sumanthd17/Spoken-to-Written",
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
 )
